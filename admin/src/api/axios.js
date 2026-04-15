0b84b00bc22bd48366d573f43824fa31ebc4c3a3
Praveen Reddy <yaminternational19@gmail.com>
2026-04-15 10:32:54 +0530

    import update

--- admin/src/api/axios.js
+++ admin/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:9000/api/v1";
+const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:9000/api/v1";
 
 const api = axios.create({
     baseURL: BASE_URL,
